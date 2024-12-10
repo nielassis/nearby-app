7fc64a68f9b0aaf06e6d910d3e64885db530e798
nielassis <nielvitorba@gmail.com>
2024-12-10 13:22:34 -0300

    feat: add expo-router

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from "react-native";
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+import { Button } from "@/components/button";
+import { router, Router } from "expo-router";
 
 export default function Index() {
   return (
@@ -13,6 +15,9 @@ export default function Index() {
     >
       <Welcome />
       <Steps />
+      <Button onPress={() => router.navigate("/home")}>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
